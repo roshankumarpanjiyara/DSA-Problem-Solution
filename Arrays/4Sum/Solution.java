e3d123a54cb0803586e970585574973f2f23352e
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2022-10-10 20:52:20 +0530

    Update Solution.java

--- Arrays/4Sum/Solution.java
+++ Arrays/4Sum/Solution.java
@@ -1,14 +1,17 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> ans = new ArrayList<>();
+        if(nums == null || nums.length == 0){
+            return ans;
+        }
         Arrays.sort(nums);
         for(int i = 0;i<nums.length-1;i++){
             for(int j = i+1;j<nums.length;j++){
-                int target2 = target - nums[i] - nums[j];
+                long target2 = (long)target - (long)nums[i] - (long)nums[j];
                 int s = j+1;
                 int e = nums.length-1;
                 while(s<e){
-                    int sum = nums[s]+nums[e];
+                    long sum = (long)nums[s]+(long)nums[e];
                     if(sum == target2){
                         ArrayList<Integer> quad = new ArrayList<>();
                         quad.add(nums[i]);
