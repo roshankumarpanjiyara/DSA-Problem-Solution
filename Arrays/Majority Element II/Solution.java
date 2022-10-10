247f5e68ac5a43d1da9da4db1825d9406c6d804b
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2022-10-10 18:15:40 +0530

    Update Solution.java

--- Arrays/Majority Element II/Solution.java
+++ Arrays/Majority Element II/Solution.java
@@ -1,3 +1,4 @@
+//approach 1
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         HashMap<Integer,Integer> check = new HashMap<>();
@@ -19,6 +20,53 @@ class Solution {
             }
         }
         // System.out.println(check);
+        return ans;
+    }
+}
+
+//approach 2
+class Solution {
+    public static ArrayList<Integer> Solve(int n, int[] nums) {
+        // code here
+        int num1 = -1;
+        int num2 = -1;
+        int count1 = 0;
+        int count2 = 0;
+        for(int i = 0;i<n;i++){
+            if(nums[i] == num1){
+                count1++;
+            }else if(nums[i] == num2){
+                count2++;
+            }else if(count1 == 0){
+                num1 = nums[i];
+                count1 = 1;
+            }else if(count2 == 0){
+                num2 = nums[i];
+                count2 = 1;
+            }else{
+                count1--;
+                count2--;
+            }
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        count1 = 0;
+        count2 = 0;
+        for(int i = 0;i<n;i++){
+            if(nums[i] == num1){
+                count1++;
+            }else if(nums[i] == num2){
+                count2++;
+            }
+        }
+        if(count1 > n/3){
+            ans.add(num1);
+        }
+        if(count2 > n/3){
+            ans.add(num2);
+        }
+        if(ans.isEmpty()){
+            ans.add(-1);
+        }
         return ans;
     }
 }
