751ed7ed0fdf661f7f2cbf413fcca3227e026001
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2022-11-11 21:08:05 +0530

    Create Solution.java

---
+++ String/Count Binary Substrings/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int prev = 0;
+        int curr = 1;
+        int ans = 0;
+        for(int i = 1;i<s.length();i++){
+            if(s.charAt(i) == s.charAt(i-1)){
+                curr++;
+            }else{
+                ans += Math.min(prev,curr);
+                prev = curr;
+                curr = 1;
+            }
+        }
+        ans += Math.min(prev,curr);
+        return ans;
+    }
+}
