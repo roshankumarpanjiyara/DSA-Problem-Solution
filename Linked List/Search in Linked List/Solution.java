441a4a83cef03a558855bd18a65a58c276700dca
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2022-12-14 21:00:14 +0530

    Create Solution.java

---
+++ Linked List/Search in Linked List/Solution.java
@@ -0,0 +1,20 @@
+/* Node of a linked list
+  class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+class Solution {
+    static boolean searchKey(int n, Node head, int key) {
+        // Code here
+        Node temp = head;
+        while(temp.next != null){
+            if(temp.data == key){
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+}
