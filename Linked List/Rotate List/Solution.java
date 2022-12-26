bda6855e21b98012a78f834df370aa6921dda3a9
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2022-12-26 21:24:17 +0530

    Create Solution.java

---
+++ Linked List/Rotate List/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode temp = head;
+        int len = 1;
+        while(temp.next != null){
+            temp = temp.next;
+            len++;
+        }
+        temp.next = head;
+        k = k%len;
+        k = len-k;
+        while(k-- > 0){
+            temp = temp.next;
+        }
+        head = temp.next;
+        temp.next = null;
+        return head;
+    }
+}
