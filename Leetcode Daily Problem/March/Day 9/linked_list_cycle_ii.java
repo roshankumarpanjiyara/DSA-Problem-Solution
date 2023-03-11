f2649897be8396b74bb8cdd0a9387b2557a5c555
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2023-03-11 21:13:20 +0530

    Create

---
+++ Leetcode Daily Problem/March/Day 9/linked_list_cycle_ii.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast){
+                ListNode curr = head;
+                while(curr != slow){
+                curr = curr.next;
+                slow = slow.next;
+            }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
