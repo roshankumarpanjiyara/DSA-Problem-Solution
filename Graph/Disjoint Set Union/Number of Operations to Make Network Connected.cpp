5af5bc870b625d0a8499fcb1e3045e2d006cfd9a
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2023-03-23 22:35:47 +0530

    Create Number of Operations to Make Network Connected.cpp

---
+++ Graph/Disjoint Set Union/Number of Operations to Make Network Connected.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> rank;
+    vector<int> parent;
+
+    int find(int x){
+        if(x == parent[x]){
+            return x;
+        }
+        return parent[x]=find(parent[x]);
+    }
+
+    void Union(int x, int y){
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if(x_parent == y_parent){
+            return;
+        }
+        if(rank[x_parent] > rank[y_parent]){
+            parent[y_parent] = x_parent;
+        }else if(rank[x_parent] < rank[y_parent]){
+            parent[x_parent] = y_parent;
+        }else{
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size() < n-1){
+            return -1;
+        }
+        parent.resize(n);
+        rank.resize(n,0);
+
+        for(int i = 0;i<n;i++){
+            parent[i] = i;
+        }
+        int components = n;
+        for(auto &vec: connections){
+            if(find(vec[0]) != find(vec[1])){
+                Union(vec[0], vec[1]);
+                components--;
+            }
+        }
+        return components-1;
+    }
+};
