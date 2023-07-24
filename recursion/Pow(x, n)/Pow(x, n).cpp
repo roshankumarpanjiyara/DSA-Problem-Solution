d9d43b4aa26bd9e1fa9bcda0c8aa73047062df37
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2023-07-24 20:22:06 +0530

    Create Pow(x, n).cpp

---
+++ recursion/Pow(x, n)/Pow(x, n).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double solve(double x, long n){
+        if(n == 0){
+            return 1;
+        }
+        if(n < 0){
+            return solve(1.0/x, -1 * n);
+        }
+        if(n % 2 == 0){
+            return solve(x * x, n/2);
+        }else{
+            return x * solve(x * x, (n-1)/2);
+        }
+    }
+    double myPow(double x, int n) {
+        return solve(x, (long)n);
+    }
+};
