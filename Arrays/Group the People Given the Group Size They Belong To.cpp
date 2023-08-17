ab26a233c1f29025cda03a48dd082eadcdd7b90e
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2023-08-17 21:41:49 +0530

    Create Group the People Given the Group Size They Belong To.cpp

---
+++ Arrays/Group the People Given the Group Size They Belong To.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        vector<vector<int>> ans;
+
+        unordered_map<int, vector<int>> mp;
+        for(int i = 0;i<groupSizes.size();i++){
+            mp[groupSizes[i]].push_back(i);
+
+            if(mp[groupSizes[i]].size() == groupSizes[i]){
+                ans.push_back(mp[groupSizes[i]]);
+                mp[groupSizes[i]].clear();
+            }
+        }
+        return ans;
+    }
+};
