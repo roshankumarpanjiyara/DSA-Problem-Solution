f1ca5a39d10af76ddc1d81b3a5b296d7ad5704cd
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2023-09-27 19:25:17 +0530

    Create Decoded String at Index.cpp

---
+++ String/Decoded String at Index.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) {
+        int n = s.length();
+        long long size = 0;
+        for(char ch : s){
+            if(isdigit(ch)){
+                size = size*(ch-'0');
+            }else{
+                size++;
+            }
+        }
+
+        for(int i = n-1;i>=0;i--){
+            k = k%size;
+            if(k == 0 && isalpha(s[i])){
+                return string(1, s[i]);
+            }
+            if(isdigit(s[i])){
+                size /= (s[i] - '0');
+            }else{
+                size--;
+            }
+        }
+
+        return "";
+    }
+};
