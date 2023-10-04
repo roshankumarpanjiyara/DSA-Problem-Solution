21b1d264b867e889a4bd8b4854f4d43f05182994
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2023-10-04 18:29:49 +0530

    Create Design HashMap.cpp

---
+++ Design/Design HashMap.cpp
@@ -0,0 +1,60 @@
+class MyHashMap {
+public:
+    int size;
+    vector<list<pair<int, int>>> buckets;
+    MyHashMap() {
+        size = 10000;
+        buckets.resize(size);
+    }
+    
+    void put(int key, int value) {
+        int bucket_no = key%size;
+        auto &chain = buckets[bucket_no];
+
+        for(auto &it : chain){
+            if(it.first == key){
+                it.second = value;
+                return;
+            }
+        }
+
+        chain.emplace_back(key, value);
+    }
+    
+    int get(int key) {
+        int bucket_no = key%size;
+        auto &chain = buckets[bucket_no];
+
+        if(chain.empty()){
+            return -1;
+        }
+
+        for(auto &it : chain){
+            if(it.first == key){
+                return it.second;
+            }
+        }
+
+        return -1;
+    }
+    
+    void remove(int key) {
+        int bucket_no = key%size;
+        auto &chain = buckets[bucket_no];
+
+        for(auto it = chain.begin(); it != chain.end();it++){
+            if(it->first == key){
+                chain.erase(it);
+                return;
+            }
+        }
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
