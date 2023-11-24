4250729da2026e2d7b90734e527e5e733200a038
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2023-11-24 18:19:30 +0530

    Create 1561. Maximum Number of Coins You Can Get.cpp

---
+++ Greedy/1561. Maximum Number of Coins You Can Get.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& piles) {
+        sort(piles.begin(), piles.end());
+        int ans = 0;
+        for(int i = piles.size()/3;i<piles.size();i+=2){
+            ans += piles[i];
+        }
+
+        return ans;
+    }
+};
