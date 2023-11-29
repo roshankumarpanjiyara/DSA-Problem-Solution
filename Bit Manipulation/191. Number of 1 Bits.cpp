b0753ee0f4ca0ac1cf9e17e9b98c4ccbcf5d0264
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2023-11-29 13:48:28 +0530

    Create 191. Number of 1 Bits.cpp

---
+++ Bit Manipulation/191. Number of 1 Bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ans = 0;
+        for(int i = 0;i<32;i++){
+            if((n & (1 << i)) != 0){
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
