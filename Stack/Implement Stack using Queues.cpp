c5ce7e856d4d4c26b16b5b53cab54886bccff701
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2024-01-29 13:09:24 +0530

    Update Implement Stack using Queues.cpp

--- Stack/Implement Stack using Queues.cpp
+++ Stack/Implement Stack using Queues.cpp
@@ -1,3 +1,4 @@
+//approach 1
 class MyStack {
 
     Queue<Integer> q1 = new LinkedList<>();
@@ -41,4 +42,55 @@ class MyStack {
  * int param_2 = obj.pop();
  * int param_3 = obj.top();
  * boolean param_4 = obj.empty();
+ */
+
+
+//approach 2
+class MyQueue {
+public:
+    int front;
+    stack<int> s1;
+    stack<int> s2;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        if(s1.empty()){
+            front = x;
+        }
+        s1.push(x);
+    }
+    
+    int pop() {
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int tp =  s2.top();
+        s2.pop();
+        return tp;
+    }
+    
+    int peek() {
+        if(!s2.empty()){
+            return s2.top();
+        }
+        return front;
+    }
+    
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
  */
