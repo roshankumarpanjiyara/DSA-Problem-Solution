14d3da54921f178607cd9f704fbfe4f5fac63f38
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2024-02-06 13:36:20 +0530

    Update Solution.java

--- String/Group Anagrams/Solution.java
+++ String/Group Anagrams/Solution.java
@@ -27,3 +27,26 @@ class Solution {
         return ans;
     }
 }
+
+//using sorting
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+        vector<vector<string>> result;
+        unordered_map<string, vector<string>> mp;
+
+        for(int i= 0;i<n;i++){
+            string temp = strs[i];
+            sort(begin(temp), end(temp));
+
+            mp[temp].push_back(strs[i]);
+        }
+
+        for(auto it : mp){
+            result.push_back(it.second);
+        }
+
+        return result;
+    }
+};
