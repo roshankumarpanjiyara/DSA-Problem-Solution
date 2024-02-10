2147430dbe528aee99cd5c51b8b44a7598f8eef5
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2024-02-10 13:29:51 +0530

    Update Solution.java

--- String/Palindromic Substrings/Solution.java
+++ String/Palindromic Substrings/Solution.java
@@ -43,3 +43,33 @@ class Solution {
         return count;
     }
 }
+
+
+//approach 2
+
+class Solution {
+public:
+    int expand(string s, int left, int right){
+        int count = 0;
+
+        while(left >= 0 && right < s.size() && s[left] == s[right]){
+            left--;
+            right++;
+            count++;
+        }
+
+        return count;
+    }
+    int countSubstrings(string s) {
+        int count = 0;
+
+        for(int i = 0;i<s.size();i++){
+            int odd = expand(s, i, i);
+            int even = expand(s, i, i + 1);
+
+            count += odd + even;
+        }
+
+        return count;
+    }
+};
