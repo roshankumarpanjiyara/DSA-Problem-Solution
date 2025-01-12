da958067938a9b926b7a1ba8b331cd76c78c93c1
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2025-01-12 21:16:23 +0530

    Update 2116. Check if a Parentheses String Can Be Valid.cpp

--- String/2116. Check if a Parentheses String Can Be Valid.cpp
+++ String/2116. Check if a Parentheses String Can Be Valid.cpp
@@ -1,3 +1,4 @@
+//stack solution
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
@@ -33,3 +34,45 @@ public:
         return open.empty();
     }
 };
+
+
+
+//constant space
+class Solution {
+    public boolean canBeValid(String s, String locked) {
+        int n = s.length();
+        if(n % 2 != 0){
+            return false;
+        }
+
+        //L to R
+        int open = 0;
+        for(int i = 0;i<n;i++){
+            if(s.charAt(i) == '(' || locked.charAt(i) == '0'){
+                open++;
+            }else{
+                open--;
+            }
+
+            if(open  < 0){
+                return false;
+            }
+        }
+
+        //R to L
+        int close = 0;
+        for(int i = n-1;i>=0;i--){
+            if(s.charAt(i) == ')' || locked.charAt(i) == '0'){
+                close++;
+            }else{
+                close--;
+            }
+
+            if(close  < 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
