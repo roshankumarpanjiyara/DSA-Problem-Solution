ab893b06f9e6d9c8dc5510c73e03c448423d13d0
Roshan Kumar Panjiyara <66662672+roshankumarpanjiyara@users.noreply.github.com>
2025-02-01 21:21:56 +0530

    Create 3151. Special Array I.cpp

---
+++ Arrays/3151. Special Array I.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0;i<n-1;i++){
+            if(nums[i] % 2 == 0 && nums[i+1] % 2 == 0){
+                return false;
+            }
+            else if(nums[i] % 2 != 0 && nums[i+1] % 2 != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
